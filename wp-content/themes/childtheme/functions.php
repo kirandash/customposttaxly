921f94e7e740d5f311971776164e23e1f7878952
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-27 21:13:38 +0530

    Include custom post types in the front-page index along with normal posts.

---
+++ wp-content/themes/childtheme/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function my_add_reviews( $query ) {
+    if ( ! is_admin() && $query->is_main_query() ) {
+        if ($query->is_home() || $query->is_search() ) {
+        $query->set( 'post_type', array( 'post', 'review' ) );
+        }
+    }
+}
+
+add_action( 'pre_get_posts', 'my_add_reviews' );
+                
+?>				
